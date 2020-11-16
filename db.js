ab121dc7ce3b854f3236075285612d8425502fd0
SeregaBiceps <58617773+SeregaBiceps@users.noreply.github.com>
2020-11-16 14:26:43 +0500

    Update db.js

--- db.js
+++ db.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-﻿//new new
-=======
-//akto?
-//new
 >>>>>>> 3ff3e8384ae3d8bcff0d19583552d94dc9759a89
 const mongoose = require('mongoose'),
   db = mongoose.createConnection('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedTopology: true}),
